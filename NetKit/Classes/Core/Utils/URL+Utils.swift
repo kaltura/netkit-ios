7865a9e94dd2922f108521a015517ffdfcb997f6
Sergey Chausov <chausov@users.noreply.github.com>
2021-08-12 15:30:35 +0300

    FEC-9808 URL  extension updated with new helper method appendingPercentEncodedQueryComponent which available on iOS/tvOS 11+ (#14)

--- NetKit/Classes/Core/Utils/URL+Utils.swift
+++ NetKit/Classes/Core/Utils/URL+Utils.swift
@@ -10,7 +10,7 @@ import Foundation
 public extension URL {
     
     func appendingQueryComponent(key: String, value: String) -> URL {
-        guard var components = URLComponents(string: absoluteString) else { return self }
+        guard var components = URLComponents(url: self, resolvingAgainstBaseURL: true) else { return self }
         
         var items: [URLQueryItem] = components.queryItems ??  []
         items.append(URLQueryItem(name: key, value: value))
@@ -18,4 +18,31 @@ public extension URL {
         
         return components.url ?? self
     }
+    
+    @available(iOS 11.0, tvOS 11.0, *)
+    func appendingPercentEncodedQueryComponent(key: String, value: String) -> URL {
+        guard var components = URLComponents(url: self, resolvingAgainstBaseURL: true) else { return self }
+        
+        var items: [URLQueryItem] = components.percentEncodedQueryItems ??  []
+        items.append(URLQueryItem(name: key, value: escape(value)))
+        components.percentEncodedQueryItems = items
+        
+        return components.url ?? self
+    }
+    
+    func escape(_ string: String) -> String {
+        string.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowedCharacterSet) ?? string
+    }
+    
+}
+
+extension CharacterSet {
+    /// RFC 3986 allowed characters.
+    public static let urlQueryAllowedCharacterSet: CharacterSet = {
+        let generalDelimitersToEncode = ":#[]@"
+        let subDelimitersToEncode = "!$&'()*+,;="
+        let encodableDelimiters = CharacterSet(charactersIn: "\(generalDelimitersToEncode)\(subDelimitersToEncode)")
+
+        return CharacterSet.urlQueryAllowed.subtracting(encodableDelimiters)
+    }()
 }
