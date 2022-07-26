a13869ea41abc8f49eca797001e2ddef2622de91
Sergey Chausov <chouzoff@gmail.com>
2022-07-26 16:04:11 +0300

    tagging script updated

--- .github/tag.sh
+++ .github/tag.sh
@@ -10,18 +10,13 @@ pod ipc spec $POD.podspec > spec.json
 TARGET_TAG=$(jq '.source.tag' --raw-output spec.json)
 VERSION=$(jq '.version' --raw-output spec.json)
 
-RELEASE_URL="https://github.com/$GITHUB_REPOSITORY/releases/tag/$TARGET_TAG"
-
-# Release to GitHub (creates tag + release notes)
 cat << EOF > post.json
 {
-  "name": "v$VERSION",
-  "body": "# $POD \n\n [TBD] \n\n ## Cocoapods install \n\`pod '$POD', '~> $VERSION'\`",
-  "tag_name": "$TARGET_TAG",
-  "target_commitish": "$GITHUB_SHA"
+  "ref": "refs/tags/$TARGET_TAG",
+  "sha": "$GITHUB_SHA"
 }
 EOF
 
-POST_URL=https://api.github.com/repos/$GITHUB_REPOSITORY/releases
+POST_URL=https://api.github.com/repos/$GITHUB_REPOSITORY/git/refs
 
 curl $POST_URL -X POST -H "Content-Type: application/json" -H "authorization: Bearer $GITHUB_TOKEN" -d@post.json
