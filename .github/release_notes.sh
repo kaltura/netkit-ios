c5eff91e5697977268a3974107fffed37fdc958a
Sergey Chausov <chausov@users.noreply.github.com>
2022-07-27 02:54:34 +0300

    Update release_notes.sh

--- .github/release_notes.sh
+++ .github/release_notes.sh
@@ -7,20 +7,8 @@ POD=$(basename $PODSPEC .podspec)
 
 pod ipc spec $POD.podspec > spec.json
 
-TARGET_TAG=$(jq '.source.tag' --raw-output spec.json)
-VERSION=$(jq '.version' --raw-output spec.json)
 POD_NAME=$(jq '.name' --raw-output spec.json)
-SUMMARY=$(jq '.summary' --raw-output spec.json)
-
-cat spec.json
-
-
-
-echo "POD info"
-echo "$POD_NAME"
-echo "$SUMMARY"
-echo "$TARGET_TAG"
-echo "$VERSION"
+VERSION=$(jq '.version' --raw-output spec.json)
 
 sed -e "s#@@RELEASE_NOTES@@#- TBA#g" -e "s#@@POD_NAME@@#$POD_NAME#g" -e "s#@@POD_PREFERRED_VERSION@@#$VERSION#g" -i '' .github/release_notes_template.md
 
