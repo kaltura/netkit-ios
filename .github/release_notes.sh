0c503d5d26c06e2ddcc180a1ec84585478bf1594
Sergey Chausov <chouzoff@gmail.com>
2022-07-27 02:10:10 +0300

    workflow updated

--- .github/release_notes.sh
+++ .github/release_notes.sh
@@ -12,6 +12,9 @@ VERSION=$(jq '.version' --raw-output spec.json)
 POD_NAME=$(jp '.name' --raw-output spec.json)
 SUMMARY=$(jp '.summary' --raw-output spec.json)
 
+cat spec.json
+
+
 
 echo "POD info"
 echo "$POD_NAME"
@@ -20,6 +23,6 @@ echo "$TARGET_TAG"
 echo "$VERSION"
 
 
-- sed -e "s#@@RELEASE_NOTES@@#- TBA#g" -e "s#@@POD_NAME@@#$POD_NAME#g" -e "s#@@POD_PREFERRED_VERSION@@#$VERSION#g" .github/release_notes_template.md
+sed -e "s#@@RELEASE_NOTES@@#- TBA#g" -e "s#@@POD_NAME@@#$POD_NAME#g" -e "s#@@POD_PREFERRED_VERSION@@#$VERSION#g" .github/release_notes_template.md
 
 cat .github/release_notes_template.md
