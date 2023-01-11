16662b4bb9684b0408baf08fe8cb3aaff30fbb5d
Sergey Chausov <chausov@users.noreply.github.com>
2023-01-11 14:19:40 +0200

    FEC-12640 SPM Added (#22)
    * SPM Added
    
    * Update pr.yml
    
    * Package minor fixes
    
    * workflow updated
    
    * workflow updated
    
    * Update ci.yml
    
    * Delete cocoapods.yml

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "KalturaNetKit",
+    platforms: [.iOS(.v11),
+                .tvOS(.v11)],
+    products: [.library(name: "KalturaNetKit",
+                        targets: ["KalturaNetKit"])],
+    dependencies: [
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", .upToNextMinor(from: "5.0.0")),
+    ],
+    targets: [.target(name: "KalturaNetKit",
+                      dependencies: ["SwiftyJSON"],
+                      path: "NetKit/Classes/Core"),
+    ]
+)
